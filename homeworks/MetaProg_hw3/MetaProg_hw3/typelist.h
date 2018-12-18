2b0f71a3093cdae10c10f29ea36e6ac3c66e93a2
Teodor Moiseev <femoiseev@gmail.com>
2018-12-18 21:37:21 +0300

    add homework 3

---
+++ homeworks/MetaProg_hw3/MetaProg_hw3/typelist.h
@@ -0,0 +1,13 @@
+//
+//  typelist.h
+//  MetaProg_hw3
+//
+//  Created by Teodor Moiseev on 18/12/2018.
+//  Copyright © 2018 Teodor Moiseev. All rights reserved.
+//
+
+#ifndef typelist_h
+#define typelist_h
+
+
+#endif /* typelist_h */
